116cf441f3c3138564519b743cc8033be9a9be07
zx12r4@yahoo.fr <zx12r4@yahoo.fr>
2018-11-28 11:32:56 +0100

    Add product route

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,17 @@
 # wsgi.py
 from flask import Flask
 app = Flask(__name__)
+from flask import jsonify
+
+PRODUCTS = [
+    { 'id': 1, 'name': 'Skello' },
+    { 'id': 2, 'name': 'Socialive.tv' }
+]
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/api/v1/products')
+def get_products():
+    return jsonify(PRODUCTS)
